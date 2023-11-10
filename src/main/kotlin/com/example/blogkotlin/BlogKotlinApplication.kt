6704a8757081382148be6f9c702ad55b4125f934
정민석 <ms.jung@fanmaum.com>
2023-11-10 21:11:08 +0900

    add res

---
+++ src/main/kotlin/com/example/blogkotlin/BlogKotlinApplication.kt
@@ -0,0 +1,12 @@
+package com.example.blogkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogKotlinApplication>(*args)
+}
+1
